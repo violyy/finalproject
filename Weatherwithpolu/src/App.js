972bcdebaf84bd48feff018854c821e9daf04659
violyy <76200560+violyy@users.noreply.github.com>
2020-12-18 23:15:00 +0700

    Update App.js

--- Weatherwithpolu/src/App.js
+++ Weatherwithpolu/src/App.js
@@ -32,7 +32,7 @@ class App extends React.Component {
     const data3 = await api_call3.json();
     const lon=data.coord.lon;
     const lat=data.coord.lat;
-    const api_call2 = await fetch(`http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${API_KEY}`)
+    const api_call2 = await fetch(`https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${API_KEY}`)
     const data2 =await api_call2.json();
       console.log(lat);
       console.log(lon);
